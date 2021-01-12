02483d7009930684be37e3748220b4d3f4648c44
baselix <34682009+baselix@users.noreply.github.com>
2021-01-12 16:09:39 +0300

    ref

--- spring-lO-homework-fe/src/app/about.component.ts
+++ spring-lO-homework-fe/src/app/about.component.ts
@@ -2,8 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
     selector: 'about-app',
-    template: `<h2 style="text-align:center">Dmitriy Gurkin's</h2>
-    <h2 style="text-align:center">Homework 9-10: SPA application</h2>
+    template: `<h2 style="text-align:center">Dmitriy Gurkin's homework 9-10:</h2>
+    <h2 style="text-align:center">Spring MVC View / Современные приложения на Spring MVC</h2>
     <br><div align="center"><img src="./assets/books.png"></div>
     `
 })
--- spring-lO-homework-fe/src/app/welcome/welcome.component.ts
+++ spring-lO-homework-fe/src/app/welcome/welcome.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'welcome-app',
-    templateUrl: './welcome.component.html'
+    templateUrl: './welcome.component.html',
     styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent { }
